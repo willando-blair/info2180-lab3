3fd1b26621b82a38a5211a3e5688c76b3e278c1c
willando-blair <willando.blair@mymona.uwi.edu>
2024-10-27 18:27:15 -0500

    Added action listener to cells

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,4 +5,46 @@ document.addEventListener('DOMContentLoaded', () => {
 	cells.forEach((div)=> {
 		div.classList.add("square");
 	});
+
+    let options = ["", "", "", "", "", "", "", "", ""];
+	let currentPlayer = "X";
+    let running = false;
+
+    startGame();
+
+	function startGame() {
+		cells.forEach((arrayItem, index, fullArray)=> {arrayItem.setAttribute("cellnum", index)});
+		cells.forEach(cell=> cell.addEventListener("click", cellSelected));
+		statusMsg.classList.add("status");
+		statusMsg.textContent = `${currentPlayer}'s turn`;
+		running = true;
+	}
+
+    function cellSelected() {
+		const cellIndex = this.getAttribute("cellnum");
+
+		if(options[cellIndex] != "" || !running) {
+			return;
+		}
+		else {
+			updateCell(this, cellIndex);
+			changePlayer();
+		}
+	}
+
+    function updateCell(cell, index) {
+		options[index] = currentPlayer;
+		cell.textContent = currentPlayer;
+		if(currentPlayer=="X") {
+			cell.classList.add("X");
+		}
+		else if(currentPlayer=="O") {
+			cell.classList.add("O");
+		}
+	}
+
+    function changePlayer() {
+		currentPlayer = (currentPlayer == "X") ? "O":"X";
+		statusMsg.textContent = `${currentPlayer}'s turn`;
+	}
 });
